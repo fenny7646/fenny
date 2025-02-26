ccfdc5a4c2f81f630c265f986bbc8610821b9424
fenny7646 <fennyvarghese1@gmail.com>
2025-02-27 04:37:28 +0530

    Updated firedata url

--- script/js/firebase.js
+++ script/js/firebase.js
@@ -1,6 +1,6 @@
 // Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
 
 // Firebase configuration (replace with your Firebase project config)
 const firebaseConfig = {
