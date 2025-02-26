f203af3278f420c385a354f2f311c165c58d94b4
fenny7646 <fennyvarghese1@gmail.com>
2025-02-27 04:13:04 +0530

    Updated firebase tokens

--- script/js/firebase.js
+++ script/js/firebase.js
@@ -3,7 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 // Firebase configuration (replace with your Firebase project config)
-const firebaseConfig = process.env.FIREBASE_AUTH;
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -15,19 +23,19 @@ const db = getFirestore(app);
 async function fetchData() {
   try {
     // Reference to the Firestore collection
-    const collectionRef = collection(db, 'portfolio'); // Replace 'users' with your collection name
+    const collectionRef = collection(db, 'portfolio'); // Replace 'portfolio' with your collection name
 
     // Fetch documents from the collection
-    const snapshot = await getDocs(collectionRef,'portfolio');
+    const snapshot = await getDocs(collectionRef);
     const data = [];
     snapshot.forEach(doc => {
       data.push({ id: doc.id, ...doc.data() }); // Include document ID and data
     });
-    console.log(data)
+    console.log(data);
     // Return the fetched data
     return data;
   } catch (error) {
     console.error('Error fetching data from Firestore:', error);
   }
 }
-export const data = await fetchData()
+export const data = await fetchData();
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,13 @@ module.exports = {
     mode: 'production', // Set the mode to 'production' or 'development'
     plugins: [
         new webpack.DefinePlugin({
-            'process.env.FIREBASE_AUTH': JSON.stringify(process.env.FIREBASE_AUTH)
+            'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+            'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+            'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+            'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+            'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+            'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+            'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
         })
     ],
     module: {
